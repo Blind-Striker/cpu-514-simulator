e4a7a234322fbb71e557a416d5db9414f278fe86
deniz_irgin@yahoo.com <deniz_irgin@yahoo.com>
2022-01-15 20:03:55 +0300

    move assembler to seperate file

---
+++ src/Swe514Cpu/Assembler.cs
@@ -0,0 +1,122 @@
+﻿using Swe514Cpu.Extensions;
+
+namespace Swe514Cpu;
+
+internal static class Assembler
+{
+    public static List<string> Assemble(string[] instructions)
+    {
+        var lineCounter = 0;
+        var labelAddressDic = new Dictionary<string, int>();
+
+        foreach (string instruction in instructions)
+        {
+            if (instruction.Trim().EndsWith(":")) // Label
+            {
+                string label = instruction.Trim().TrimEnd(':');
+                labelAddressDic.Add(label, lineCounter * 3);
+                continue;
+            }
+
+            lineCounter++;
+        }
+
+        var registers = new Dictionary<string, int>
+        {
+            { "PC", 0 },
+            { "A", 1 },
+            { "B", 2 },
+            { "C", 3 },
+            { "D", 4 },
+            { "E", 5 },
+            { "S", 6 }
+        };
+
+
+        var binInstructions = new List<string>();
+
+        foreach (string instruction in instructions)
+        {
+            if (instruction.Trim().EndsWith(":")) // Label
+            {
+                continue;
+            }
+
+            if (instruction.Trim() == "HALT")
+            {
+                InstructionOperand haltOp = InstructionOperand.GetByName("HALT");
+                string haltOpCodeBin = haltOp.CodeBin;
+                string haltAddModeBin = ((int)AddressingModes.Immediate).ToBin(2);
+                string haltOperandBin = 0.ToBin(16);
+
+                string haltInstruction = $"{haltOpCodeBin}{haltAddModeBin}{haltOperandBin}".ToHex();
+
+                Console.WriteLine(haltInstruction);
+                binInstructions.Add(haltInstruction);
+
+                continue;
+            }
+
+            string[] spl = instruction.Trim().Split(' ');
+            string opCode = spl[0];
+            string operand = spl[1];
+
+            InstructionOperand instOperand = InstructionOperand.GetByName(opCode);
+
+            int addressingMode;
+            int operandValue;
+
+            if (instOperand == null)
+            {
+                throw new Exception($"OpCode '{opCode}' not found");
+            }
+
+            if (operand.Length == 1 && registers.ContainsKey(operand))
+            {
+                addressingMode = (int)AddressingModes.Register;
+                operandValue = registers[operand];
+            }
+            else if ((operand.StartsWith("'") || operand.StartsWith("‘")) && (operand.EndsWith("'") || operand.EndsWith("’")))
+            {
+                addressingMode = (int)AddressingModes.Immediate;
+                operandValue = operand.Replace("'", "").Replace("‘", "")[0];
+            }
+            else if (int.TryParse(operand, out int numericValue))
+            {
+                addressingMode = (int)AddressingModes.Immediate;
+                operandValue = numericValue;
+            }
+            else if (operand.StartsWith("[") && operand.EndsWith("]"))
+            {
+                string registerOrMemoryAddress = operand.Replace("[", "").Replace("]", "");
+
+                if (operand.Length == 3)
+                {
+                    addressingMode = (int)AddressingModes.MemoryAddressRegister;
+                    operandValue = registers[registerOrMemoryAddress];
+                }
+                else
+                {
+                    addressingMode = (int)AddressingModes.MemoryAddress;
+                    operandValue = int.Parse(registerOrMemoryAddress);
+                }
+            }
+            else
+            {
+                addressingMode = (int)AddressingModes.Immediate;
+                operandValue = labelAddressDic[operand];
+            }
+
+            string opCodeBin = instOperand.CodeBin;
+            string addressModeBin = addressingMode.ToBin(2);
+            string operandBin = operandValue.ToBin(16);
+
+            string instructionBin = $"{opCodeBin}{addressModeBin}{operandBin}".ToHex();
+
+            Console.WriteLine(instructionBin);
+            binInstructions.Add(instructionBin);
+        }
+
+        return binInstructions;
+    }
+}
--- src/Swe514Cpu/Program.cs
+++ src/Swe514Cpu/Program.cs
@@ -1,97 +1,53 @@
 ﻿using Swe514Cpu;
-using Swe514Cpu.Extensions;
 
-string[] progAsm = File.ReadAllLines("./prog.txt");
-
-var lineCounter = 0;
-var labelAddressDic = new Dictionary<string, int>();
-
-foreach (string instruction in progAsm)
+if (args.Length == 0)
 {
-    if (instruction.Trim().EndsWith(":")) // Label
-    {
-        string label = instruction.Trim().TrimEnd(':');
-        labelAddressDic.Add(label, lineCounter * 3);
-        continue;
-    }
-
-    lineCounter++;
+    Console.WriteLine("Supported commands: assemble, execute");
+    return;
 }
 
-var registers = new Dictionary<string, int>
+string command = args[0];
+
+if (command == "assemble")
 {
-    { "PC", 0 },
-    { "A", 1 },
-    { "B", 2 },
-    { "C", 3 },
-    { "D", 4 },
-    { "E", 5 },
-    { "S", 6 }
-};
+    if (args.Length != 5)
+    {
+        Console.WriteLine("Invalid argument, please execute program with following example, assemble --file file.txt --output ./output.bin");
+        return;
+    }
 
+    string inputPath = args[2];
+    string outputPath = args[4];
 
-foreach (string instruction in progAsm)
+    if (inputPath == null)
+    {
+        Console.WriteLine("Please enter a valid input path");
+        return;
+    }
+
+    if (!File.Exists(inputPath))
+    {
+        Console.WriteLine($"File not exists: {inputPath}");
+        return;
+    }
+
+    if (outputPath == null)
+    {
+        Console.WriteLine("Please enter a valid output path");
+        return;
+    }
+
+    string[] programAssemblies = File.ReadAllLines(inputPath);
+
+    List<string> binInstructions = Assembler.Assemble(programAssemblies);
+
+    File.WriteAllLines(outputPath, binInstructions);
+}
+else if (command == "execute")
 {
-    if (instruction.Trim().EndsWith(":")) // Label
-    {
-        continue;
-    }
 
-    string[] spl = instruction.Trim().Split(' ');
-    string opCode = spl[0];
-    string operand = spl[1];
-
-    InstructionOperand instOperand = InstructionOperand.GetByName(opCode);
-
-    if (instOperand == null)
-    {
-        throw new Exception($"OpCode '{opCode}' not found");
-    }
-
-    int addressingMode;
-    int operandValue;
-
-    if (operand.Length == 1 && registers.ContainsKey(operand))
-    {
-        addressingMode = (int)AddressingModes.Register;
-        operandValue = registers[operand];
-    }
-    else if ((operand.StartsWith("'") || operand.StartsWith("‘")) && (operand.EndsWith("'") || operand.EndsWith("’")))
-    {
-        addressingMode = (int)AddressingModes.Immediate;
-        operandValue = operand.Replace("'", "").Replace("‘", "")[0];
-    }
-    else if (int.TryParse(operand, out int numericValue))
-    {
-        addressingMode = (int)AddressingModes.Immediate;
-        operandValue = numericValue;
-    }
-    else if (operand.StartsWith("[") && operand.EndsWith("]"))
-    {
-        string registerOrMemoryAddress = operand.Replace("[", "").Replace("]", "");
-
-        if (operand.Length == 3)
-        {
-            addressingMode = (int)AddressingModes.MemoryAddressRegister;
-            operandValue = registers[registerOrMemoryAddress];
-        }
-        else
-        {
-            addressingMode = (int)AddressingModes.MemoryAddress;
-            operandValue = int.Parse(registerOrMemoryAddress);
-        }
-    }
-    else
-    {
-        addressingMode = (int)AddressingModes.Immediate;
-        operandValue = labelAddressDic[operand];
-    }
-
-    string opCodeBin = instOperand.CodeBin;
-    string addressModeBin = addressingMode.ToBin(2);
-    string operandBin = operandValue.ToBin(16);
-
-    string instructionBin = $"{opCodeBin}{addressModeBin}{operandBin}".ToHex();
-
-    Console.WriteLine(instructionBin);
+}
+else
+{
+    Console.WriteLine($"Invalid command: {command}");
 }
